94fc4dfb946b6d7e4466c1372354dfe5e2324136
Sonny <sonny@Sonny.local>
2023-02-21 13:21:29 +0300

    the genuine cancellation has been added up

--- src/HttpLoader.elm
+++ src/HttpLoader.elm
@@ -8,6 +8,7 @@ import Process exposing (sleep)
 import Task exposing (perform)
 import Http
 import Platform.Cmd exposing (batch)
+import Time
 
 main =
   Browser.element
@@ -19,15 +20,20 @@ main =
 
 type URLState = Idle | InProgress
 
-type alias URL = { url : String, state : URLState, content : Maybe String }
+type alias URL = { url : String, state : URLState, content : Maybe String, reqTracker : Maybe String }
 
 init : () -> (URL, Cmd Msg)
 init _ =
-  ( {url = "", state = Idle, content = Nothing}
+  ( {url = "", state = Idle, content = Nothing, reqTracker = Nothing}
   , Cmd.none
   )
 
-type Msg = FillUrl String | Start | Cancel | UrlContent (Result Http.Error String)
+type Msg =
+     FillUrl String
+   | Start
+   | Cancel
+   | UrlContent (Result Http.Error String)
+   | ReqTracker Time.Posix
 
 subscriptions : URL -> Sub Msg
 subscriptions model = Sub.none
@@ -40,30 +46,45 @@ update msg model =
      if not (String.isEmpty (.url model))
      then
       ( {model | state = InProgress}
-      , Http.get
-         {  url = .url model
-            , expect = Http.expectString UrlContent
-         }
+      , perform ReqTracker Time.now
       )
       else (model, Cmd.none)
-    Cancel -> ({model | state = Idle, url = ""}, Cmd.none)
+    Cancel ->
+    -- request 1676974750184 has been cancelled
+     ( {model | state = Idle, url = "", content = Maybe.map (\tr -> "request " ++ tr ++ " has been cancelled") (.reqTracker model) }
+     , case .reqTracker model of
+        Just tr -> Http.cancel tr
+        _ -> Cmd.none
+     )
     UrlContent result ->
       case result of
         Ok text ->
           ({model | content = Just text, state = Idle, url = ""}, Cmd.none)
         Err _ ->
           ({model | content = Nothing, state = Idle, url = ""}, Cmd.none)
+    ReqTracker tm ->
+      ( {model | reqTracker = Just (String.fromInt (Time.posixToMillis tm))}
+      , Http.request
+        { method = "GET"
+        , headers = []
+        , url = .url model
+        , body = Http.emptyBody
+        , expect = Http.expectString UrlContent
+        , timeout = Nothing
+        , tracker = Just (String.fromInt (Time.posixToMillis tm))
+        }
+      )
 
 view : URL -> Html Msg
-view { url, state, content } =
+view { url, state, content, reqTracker } =
   div []
    [
       input [ type_ "text", placeholder "input URL here", onInput FillUrl ] []
-    , if state == Idle then div [] [] else div [ style "color" "black" ] [ text "download is in progress..." ]
+    , if state == Idle then div [] [] else div [ style "color" "black" ] [ text ("id:" ++ Maybe.withDefault "" reqTracker ++  "download is in progress...") ]
     , button [ onClick Start, disabled (state == InProgress) ] [ text "download" ]
     , button [ onClick Cancel, disabled (state == Idle) ] [ text "cancel" ]
     , pre [] [ text
        (case content of
          Just s -> s
-         _ -> "error") ]
+         _ -> "") ]
    ]
